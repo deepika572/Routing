b13c0fd53af6dc7607270462902592f2f57172ce
Deepika <prasaddeepika88@gmail.com>
2021-02-20 15:21:35 +0530

    routing-test

---
+++ pages/bangalore/car-denting-painting.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Link from "next/link";
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+class ScrollableTabsButtonAuto extends React.Component {
+  state = {
+    value: 1,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+          >
+           <Link href="/bangalore/general-car-service"><Tab label="Item One" /></Link> 
+           <Link href="/bangalore/car-denting-painting"><Tab label="Item Two" /></Link> 
+           <Link href="/guwahati/general-car-service"><Tab label="Item Three" /></Link> 
+           <Link href="/guwahati/car-denting-painting"><Tab label="Item Four" /></Link> 
+          </Tabs>
+        </AppBar>
+        {value === 1 && <TabContainer>Item Two</TabContainer>}
+      </div>
+    );
+  }
+}
+
+ScrollableTabsButtonAuto.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ScrollableTabsButtonAuto);
